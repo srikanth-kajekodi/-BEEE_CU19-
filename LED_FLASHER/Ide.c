3c2527dfa4d1f7763170f07bbb67cca75f6615be
srikanth-kajekodi <53625471+srikanth-kajekodi@users.noreply.github.com>
2019-09-19 18:15:25 +0530

    Add files via upload

---
+++ LED_FLASHER/Ide.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+    void setup()
+{
+  pinMode(9, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(9, HIGH);
+  delay(1200); // Wait for 1200 millisecond(s)
+  digitalWrite(9, LOW);
+  delay(1200); // Wait for 1200 millisecond(s)
+}
+
+
